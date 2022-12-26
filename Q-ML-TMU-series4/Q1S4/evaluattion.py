b9fde8f06e04b3340c4fd8a8d0c278afe614089e
Seyed Ali Shahrokhi <alishahrokhi@icloud.com>
2022-12-26 11:35:19 +0330

    First Commit

---
+++ Q-ML-TMU-series4/Q1S4/evaluattion.py
@@ -0,0 +1,20 @@
+def evaluate( trainX , trainY , testX , testY):
+    print("\n#################\n")
+
+    ## YOUR CODE
+
+    rmse_tr = ## YOUR CODE
+    rmse_te = ## YOUR CODE
+
+    mae_tr = ## YOUR CODE
+    mae_te = ## YOUR CODE
+
+    print("RMSE train:", rmse_tr)
+    print("RMSE test:", rmse_te)
+
+    print("MAE train:", mae_tr)
+    print("MAE test:", mae_te)
+
+    return rmse_tr , rmse_te ,mae_tr , mae_te
+
+evaluate(...)
